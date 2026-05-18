05edcbeedc38ad758e7640b2db1a25fb07024073
班定富 <16314548+ye-shuanghua@user.noreply.gitee.com>
2026-05-18 13:38:58 +0800

    修复weindows循环依赖的问题

--- Chat-User/src/main/java/chat/chatuser/controller/UserController.java
+++ Chat-User/src/main/java/chat/chatuser/controller/UserController.java
@@ -10,7 +10,6 @@ import chat.chatuser.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.*;
 import chat.chatcommon.dto.Result;
 import org.springframework.web.multipart.MultipartFile;
